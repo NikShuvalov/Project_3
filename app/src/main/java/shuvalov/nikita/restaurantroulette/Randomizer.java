60aad48691eacd0468d4608e4fab22ec2b9cb73c
NikShuvalov <nshuvalov89@gmail.com>
2016-11-30 10:51:21 -0500

    Added placeholder classes for randomizer, and yelpsearch

---
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/Randomizer.java
@@ -0,0 +1,13 @@
+package shuvalov.nikita.restaurantroulette;
+
+/**
+ * Created by NikitaShuvalov on 11/30/16.
+ */
+
+public class Randomizer {
+
+    /**
+     * Picks n Random businesses from a list based on parameters.
+     * @param List<Business> String... preferences
+     */
+}
---
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/YelpSearch.java
@@ -0,0 +1,12 @@
+package shuvalov.nikita.restaurantroulette;
+
+/**
+ * Created by NikitaShuvalov on 11/30/16.
+ */
+
+public class YelpSearch {
+
+    /**
+     * Typical yelp search logic goes in here.
+     */
+}
