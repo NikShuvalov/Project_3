3e7f30f0d2d2bd077318e1040de5a0c3267dc44e
NikShuvalov <nshuvalov89@gmail.com>
2016-11-30 10:16:14 -0500

    Added Yelp API constants information

---
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/YelpAppConstants.java
@@ -0,0 +1,14 @@
+package shuvalov.nikita.restaurantroulette;
+
+/**
+ * Created by NikitaShuvalov on 11/30/16.
+ */
+
+public class YelpAppConstants {
+    public static final String YELP_BEARER_TOKEN = "NA8-lx7QOQKK1e8u6CZqNXqli320QXYVoXMELvv5BVwdoUDPWY7HrpeRb7EHWwygPrBVRxESrRGxtr1PxGOY0N2i24vI6_ASJ7NQsLyu37ARsT3X3W02HGmi8xQ2WHYx";
+    public static final String YELP_CLIENT_ID = "9YI_a1fQDy1CtSBiZsq6Yw";
+    public static final String YELP_APP_SECRET = "edmHqYlFEY4SxJWqkgM76RmkVaXnRyr6UUdc9jXzQglM8TCELkWRGN37CBG3Ztpy";
+    public static final String YELP_TOKEN_BASE_URL = "https://api.yelp.com/oauth2/";
+    public static final String YELP_SEARCH_BASE_URL = "https://api.yelp.com/v3/businesses/";
+
+}
