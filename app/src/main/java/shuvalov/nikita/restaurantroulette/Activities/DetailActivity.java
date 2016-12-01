186b019e7063c6dc13c0ed8ba8f899f3fda34ad5
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2016-12-01 15:55:55 -0500

    added map fragment

--- app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/DetailActivity.java
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/DetailActivity.java
@@ -1,23 +1,38 @@
 package shuvalov.nikita.restaurantroulette.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
 import shuvalov.nikita.restaurantroulette.R;
 import shuvalov.nikita.restaurantroulette.YelpResources.YelpObjects.Business;
 import shuvalov.nikita.restaurantroulette.YelpResources.YelpObjects.Location;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final int REQUEST_CODE_LOCATION = 1;
+
     public TextView mBusinessName, mPrice,
             mPhoneNumber, mAddress, mOpenOrClosed;
-    public ImageView mBusinessImage, mShare, mPhoneButton, mMapFrame,
+    public ImageView mBusinessImage, mShare, mPhoneButton,
             mFirstStar, mSecondStar, mThirdStar, mFourthStar, mFifthStar;
     public Business mBusiness;
 
@@ -36,19 +51,19 @@ public class DetailActivity extends AppCompatActivity {
         mBusinessImage = (ImageView) findViewById(R.id.business_image);
         mShare = (ImageView) findViewById(R.id.share_image_view);
         mPhoneButton = (ImageView) findViewById(R.id.call_image_view);
-        mMapFrame = (ImageView) findViewById(R.id.map_frame);
 
         // Review Stars
-        // TODO: Create Logic for Review Stars
         mFirstStar = (ImageView) findViewById(R.id.first_star);
         mSecondStar = (ImageView) findViewById(R.id.second_star);
         mThirdStar = (ImageView) findViewById(R.id.third_star);
         mFourthStar = (ImageView) findViewById(R.id.fourth_star);
         mFifthStar = (ImageView) findViewById(R.id.fifth_star);
 
+        // TODO: Create Logic for Review Stars
+
         // Picasso method to get images for each business
         Picasso.with(this)
-                .load("") // TODO: ADD URL
+                .load("http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg") // TODO: ADD URL
                 .into(mBusinessImage);
 
         // TODO: add bindDataToView() method
@@ -65,15 +80,47 @@ public class DetailActivity extends AppCompatActivity {
         });
 
         // TODO: add OnClickListener for Share ImageView
+
+        // Map Setup
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        Button button = (Button) findViewById(R.id.map_activity_intent);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void bindDataToView(Business business){
+    public void bindDataToView(Business business) {
         mBusinessName.setText(business.getName());
         mPrice.setText(business.getPrice());
         mPhoneNumber.setText(business.getPhone());
 
         Location location = business.getLocation();
         mAddress.setText(location.getAddress1() + " " + location.getCity());
-        mOpenOrClosed.setText((business.getIsClosed())?"Closed":"Open");
+        mOpenOrClosed.setText((business.getIsClosed()) ? "Closed" : "Open");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng ourLocation = new LatLng(40.73873873873874, -73.97987613997012);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE_LOCATION);
+
+        } else if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            googleMap.setMyLocationEnabled(true);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ourLocation, 13));
+
+            googleMap.addMarker(new MarkerOptions()
+                    .title("General Assembly")
+                    .snippet("Where We Study ADI")
+                    .position(ourLocation));
+        }
     }
 }
