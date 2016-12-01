2393314aecd8f6d388b4bc47a81d667d918d5b59
NikShuvalov <nshuvalov89@gmail.com>
2016-12-01 11:08:59 -0500

    Added more buttons to navigate to other activities for debugging

--- app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/DebugActivity.java
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/DebugActivity.java
@@ -9,23 +9,45 @@ import android.widget.Button;
 import shuvalov.nikita.restaurantroulette.R;
 
 public class DebugActivity extends AppCompatActivity {
-    Button mDebug;
+    Button mProfile, mShare, mSearch, mDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
 
+
         findViews();
-        mDebug.setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), UserSettingsActivity.class);
-                startActivity(intent);
+                Intent intent = null;
+                if (view.equals(mProfile)) {
+                    intent = new Intent(view.getContext(), UserSettingsActivity.class);
+                } else if (view.equals(mShare)) {
+                    intent = new Intent(view.getContext(), ShareActivity.class);
+                } else if (view.equals(mSearch)) {
+                    intent = new Intent(view.getContext(), SearchActivity.class);
+                } else if (view.equals(mDetail)) {
+                    intent = new Intent(view.getContext(), DetailActivity.class);
+                }
+                if (intent != null) {
+                    startActivity(intent);
+                }
             }
-        });
+        };
+        mProfile.setOnClickListener(onClickListener);
+        mShare.setOnClickListener(onClickListener);
+        mSearch.setOnClickListener(onClickListener);
+        mDetail.setOnClickListener(onClickListener);
+
+
     }
     public void findViews(){
-        mDebug = (Button)findViewById(R.id.profile_settings_Debug);
+        mProfile = (Button)findViewById(R.id.profile_settings_Debug);
+        mShare = (Button)findViewById(R.id.share_activity);
+        mSearch = (Button)findViewById(R.id.basic_search);
+        mDetail = (Button)findViewById(R.id.detail_activity);
     }
 }
