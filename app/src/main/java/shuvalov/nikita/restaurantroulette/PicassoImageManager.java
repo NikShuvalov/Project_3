df6ff942c88baaea0fef9e1e25addb8929ae638a
NikShuvalov <nshuvalov89@gmail.com>
2016-12-02 11:55:36 -0500

    Added some documentation notes to the picassoImageManager

--- app/src/main/java/shuvalov/nikita/restaurantroulette/PicassoImageManager.java
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/PicassoImageManager.java
@@ -16,12 +16,28 @@ public class PicassoImageManager {
     private ImageView mImageView;
     private RequestCreator mRequestCreator;
 
+    /**
+     * The Manager takes in an imageview and its context to create a picassoImageManager object.
+     * Then use the setImageFromUrl method to load an image from that url and updated the imageview with that image.
+     *
+     * Create a new picassoImageManager for each image.
+     * //FixMe: Try to make the manager change parameters instead of having to make new manager for each call.
+     * FixMe:... Only issue I can see with that is that if imageView and context are changed before image is loaded it would load the image into the wrong imageView
+     *
+     * @param context
+     * @param imageView
+     */
+
 
     public PicassoImageManager(Context context, ImageView imageView){
         mContext = context;
         mImageView = imageView;
     }
 
+    /**
+     * Takes in the url of image to be loaded.
+     * @param imageUrl
+     */
     public void setImageFromUrl(final String imageUrl){
 
         AsyncTask asyncTask = new AsyncTask<Void, Void, Void>(){
