30e0905629bea9a9d7e10bde7d032a65609f75aa
jwells6556 <jwells6556@gmail.com>
2016-12-05 15:46:23 -0500

    Google location services working

--- app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/RouletteActivity.java
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/RouletteActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,10 +18,15 @@ import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import shuvalov.nikita.restaurantroulette.GoogleResources.GoogleAPI;
 import shuvalov.nikita.restaurantroulette.OurAppConstants;
 import shuvalov.nikita.restaurantroulette.R;
 import shuvalov.nikita.restaurantroulette.RecyclerViewAdapters.RouletteActivityRecyclerAdapter;
@@ -32,14 +39,20 @@ import static shuvalov.nikita.restaurantroulette.OurAppConstants.SHARED_PREF_NUM
 import static shuvalov.nikita.restaurantroulette.OurAppConstants.SHARED_PREF_PRICING;
 import static shuvalov.nikita.restaurantroulette.OurAppConstants.SHARED_PREF_RADIUS;
 import static shuvalov.nikita.restaurantroulette.OurAppConstants.SHARED_PREF_RATING;
+import static shuvalov.nikita.restaurantroulette.OurAppConstants.USER_LAST_LAT;
+import static shuvalov.nikita.restaurantroulette.OurAppConstants.USER_LAST_LOCATION;
+import static shuvalov.nikita.restaurantroulette.OurAppConstants.USER_LAST_LON;
 import static shuvalov.nikita.restaurantroulette.OurAppConstants.USER_PREFERENCES;
 
-public class RouletteActivity extends AppCompatActivity {
+public class RouletteActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     private RecyclerView mRecyclerView;
     private RouletteActivityRecyclerAdapter mAdapter;
     private FloatingActionButton mRouletteButton, mSettingsButton;
     private List<Business> mRouletteList;
     private EditText mRouletteQuery;
+    private Location mLocation;
+    String mLat, mLon;
+    public GoogleApiClient mGoogleApiClient;
 
 
 
@@ -48,6 +61,10 @@ public class RouletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roulette);
+
+        GoogleAPI googleAPI = new GoogleAPI();
+        mGoogleApiClient = googleAPI.callGoogleLocApi(RouletteActivity.this);
+
 
         final InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY,0);
@@ -82,11 +99,8 @@ public class RouletteActivity extends AppCompatActivity {
 
                     long radiusSavedPosition = sharedPreferences.getLong(SHARED_PREF_RADIUS, -1);
 
-                    Location mockLocation = new Location(LOCATION_SERVICE);
-                    mockLocation.setLongitude(OurAppConstants.GA_LONGITUDE);
-                    mockLocation.setLatitude(OurAppConstants.GA_LATITUDE);
 
-                    YelpAPI yelpAPI = new YelpAPI(RouletteActivity.this, mockLocation);
+                    YelpAPI yelpAPI = new YelpAPI(RouletteActivity.this, mLocation);
 
                     String query = mRouletteQuery.getText().toString();
 
@@ -114,5 +128,45 @@ public class RouletteActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
 
+        mLat = new GoogleAPI().getUserLat(mGoogleApiClient);
+        mLon= new GoogleAPI().getUserLon(mGoogleApiClient);
+
+
+        mLocation = new Location(LOCATION_SERVICE);
+        mLocation.setLongitude(Double.parseDouble(mLon));
+        mLocation.setLatitude(Double.parseDouble(mLat));
+
+        SharedPreferences sharedPreferences = getSharedPreferences(USER_LAST_LOCATION,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USER_LAST_LAT, mLat);
+        editor.putString(USER_LAST_LON, mLon);
+        editor.commit();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "lol failure", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
 }
