4dac6b0388ac274f5887d4b42061cca5b21689ae
serkanbal <serkanbaltr@gmail.com>
2016-12-06 17:37:57 -0500

    before merge

--- app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/UserSettingsActivity.java
+++ app/src/main/java/shuvalov/nikita/restaurantroulette/Activities/UserSettingsActivity.java
@@ -286,7 +286,7 @@ public class UserSettingsActivity extends AppCompatActivity implements GoogleApi
                     JobInfo periodicJobInfo = new JobInfo.Builder(OurAppConstants.PERIODIC_JOB_ID,
                             new ComponentName(UserSettingsActivity.this, YelpJobService.class))
                             .setExtras(periodicPersistableBundle)
-                            .setPeriodic(10000) //RUN IT EVERY 10 secs
+                            .setPeriodic(900000) //RUN IT EVERY 15 mins
                             .build();
 
                     JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
@@ -320,7 +320,7 @@ public class UserSettingsActivity extends AppCompatActivity implements GoogleApi
 
     protected void startLocationUpdates() {
         LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
+        locationRequest.setInterval(300000); //UPDATE EVERY 5 MINS
         LocationServices.FusedLocationApi.requestLocationUpdates(
                 mGoogleApiClient, locationRequest, this);
     }
